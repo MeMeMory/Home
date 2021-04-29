20d65bd2f5092a318fb163911f52bc98f154db1d
MeMeMory <68325076+MeMeMory@users.noreply.github.com>
2021-04-29 14:36:39 +0300

    add 2 new sections
    add section "about_us", section "latest_works"

---
+++ js/slider.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper(".swiper-container",{slidesPerView:4,spaceBetween:30,slidesPerGroup:1,loop:!0,loopFillGroupWithBlank:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
